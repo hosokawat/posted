d758c967895f8addd1a74c7c92b74b46d81d8108
hosokawa.takahiro <vwxyzzy@gmail.com>
2018-05-10 21:44:51 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,144 @@
+var fs = require('fs');
+var _ = require('lodash');
+
+var previewTemplate = fs.readFileSync('./private/previewTemplate.html.txt').toString();
+var editorTemplate = fs.readFileSync('./private/index.html').toString();
+
+var express = require('express');
+var app = express();
+
+var Datastore = require('nedb');
+var db = new Datastore({ filename: './posted.db', autoload: true });
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  var doc = createDocument();
+  db.insert(doc, function (err, newDocs) {
+    res.redirect(`/${newDocs['_id']}`);
+  });
+});
+app.use(express.static('public'));
+
+function createDocument() {
+  return { html: '', css: '', js: '', libs: [], origin_id: '', hide: '0' }
+}
+
+function generatePreview(file_code, doc) {
+  var libs = '';
+  _.forEach(doc['libs'], function (lib) {
+    libs = libs + `<script src="${lib}"></script>`
+  })
+  return previewTemplate.replace(':html', doc['html'])
+    .replace(':css', doc['css'])
+    .replace(':js', doc['js'])
+    .replace(':libs', libs);
+}
+
+function commit(origin_doc) {
+  var doc = createDocument();
+  doc['html'] = origin_doc['html'];
+  doc['css'] = origin_doc['css'];
+  doc['js'] = origin_doc['js'];
+  doc['libs'] = origin_doc['libs'];
+  doc['origin_id'] = origin_doc['_id'];
+  return doc;
+}
+
+app.get('/:file_code/commit', function (req, res) {
+  var file_code = req.params.file_code;
+  db.findOne({ _id: file_code }, function (err, origin_doc) {
+    if (!doc) { return }
+    var doc = commit(origin_doc);
+    db.insert(doc, function (err, newDocs) {
+      res.redirect(`/${newDocs['_id']}`);
+    });
+  });
+});
+
+app.get('/:file_code.preview', function (req, res) {
+  var file_code = req.params.file_code;
+  db.findOne({ _id: file_code }, function (err, doc) {
+    if (!doc) { return }
+    res.send(generatePreview(file_code, doc));
+  });
+});
+
+app.post('/:file_code.libs', function (req, res) {
+  var data = req.body.data;
+  var remove = req.body.remove;
+
+  var file_code = req.params.file_code;
+  db.findOne({ _id: file_code }, function (err, doc) {
+    if (!doc) { return }
+    var libs = doc.libs;
+    if (remove === 'true') {
+      libs = _.without(libs, data);
+    } else {
+      libs.push(data);
+    }
+    db.update({ _id: req.params.file_code }, { $set: { libs: libs } }, function (err, numReplaced) {
+      res.send(libs.join(','));
+    });
+  });
+});
+
+
+app.post('/:file_code.:file_ext', function (req, res) {
+  var data = req.body.data;
+  var comm = {};
+  comm[req.params.file_ext] = req.body.data;
+  db.update({ _id: req.params.file_code }, { $set: comm }, function (err, numReplaced) {
+    res.send(`ok`);
+  });
+});
+
+function generateEditor(file_code, doc) {
+  return editorTemplate.replace(':html', doc['html'])
+    .replace(':css', doc['css'])
+    .replace(':js', doc['js'])
+    .replace(/:pv/g, `/${file_code}.preview`)
+    .replace(':commit', `/${file_code}/commit`);
+}
+
+app.get('/:file_code.libs', function (req, res) {
+  var file_code = req.params.file_code;
+  db.findOne({ _id: file_code }, function (err, doc) {
+    if (!doc) { return }
+    res.send(doc.libs.join(','));
+  });
+});
+
+app.get('/:file_code', function (req, res) {
+  var file_code = req.params.file_code;
+  db.findOne({ _id: file_code }, function (err, doc) {
+    if (!doc) { return }
+    res.send(generateEditor(file_code, doc));
+  });
+});
+
+app.get('/:file_code/history', function (req, res) {
+  function docHistory(doc, history = []) {
+    history.push({ id: doc['_id'] });
+    if (doc['origin_id']) {
+      db.findOne({ _id: doc['origin_id'] }, function (err, origin_doc) {
+        docHistory(origin_doc, history);
+      });
+    } else {
+      res.json(history);
+    }
+  }
+  var file_code = req.params.file_code;
+  db.findOne({ _id: file_code }, function (err, doc) {
+    if (!doc) { return }
+    docHistory(doc);
+  });
+
+});
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
---
+++ public/js/index.js
@@ -0,0 +1,161 @@
+$(function () {
+    var file_code = location.pathname.split('/')[1];
+
+    function generateLibList() {
+        $dropdownMenu = $('<div class="dropdown-menu" role="menu"></div>');
+        var firstSkip = true;
+        _.forEach(libs, function (value, key) {
+            if (!firstSkip) {
+                $dropdownMenu.append('<div class="dropdown-divider"></div>');
+            }
+            firstSkip = false;
+            $dropdownMenu.append($(`<h5 class="dropdown-header">${key}</h5>`));
+            _.forEach(value, function (v) {
+                $dropdownMenu.append($(`<a class="dropdown-item addLibListSelect" href="#" data-url='${v['url']}'>${v['name']}</a>`));
+            });
+        });
+
+        $('#addLibList .dropdown-menu').html($dropdownMenu.html())
+
+    }
+    $('#addLibList  .dropdown-toggle').click(function () {
+        $('#addLibList .dropdown-menu').toggleClass('show');
+        return false;
+    })
+
+    $.getJSON("libs.json", function (data) {
+        libs = _.chain(data).orderBy('order').groupBy('category').value();;
+        generateLibList();
+    });
+    function reloadPreview() {
+        if ($('#autoReload.active').length == 0) return;
+        var $pv = $(`<iframe style="display:inline; width:100%;position:absolute;top:0;left:0;z-index:0;" src='${file_code}.preview'></iframe>`)
+        $('#preview').remove('iframe');
+        $('#preview').append($pv);
+    }
+    $('#toggleContent .nav-link').click(function () {
+        $(this).toggleClass('active');
+        var ext = $(this).data('ext');
+        var $content = $("#content").find(`[data-ext="${ext}"]`);
+        if ($(this).hasClass('active')) {
+            $content.show();
+        } else {
+            $content.hide();
+        }
+    });
+    $('#content textarea').on('keyup keypress', function () {
+        var ext = $(this).closest('div.col').data('ext');
+        console.log(`/${file_code}.${ext}`);
+        $.ajax({
+            url: `${file_code}.${ext}`,
+            type: 'post',
+            dataType: 'json',
+            data: { data: $(this).val() },
+            dataType: 'text'
+        }).done(function (data) {
+            reloadPreview();
+        });
+    });
+
+    function updateLibUrl(url, remove) {
+        $.ajax({
+            url: `/${file_code}.libs`,
+            type: 'post',
+            dataType: 'json',
+            data: { data: url, remove: remove },
+            dataType: 'text'
+        }).done(function (data) {
+            generateActiveLibList(data.split(','));
+            reloadPreview();
+        });
+    }
+
+    function loadLibUrl() {
+        $.ajax({
+            url: `/${file_code}.libs`,
+            type: 'get',
+            dataType: 'json',
+            data: {},
+            dataType: 'text'
+        }).done(function (data) {
+            generateActiveLibList(data.split(','));
+        });
+    }
+
+    loadLibUrl();
+    $('#addLibUrlSubmit').click(function (e) {
+        e.preventDefault();
+        var url = $('#addLibUrl').val();
+        updateLibUrl(url, false);
+        $('#addLibUrl').val('');
+
+        $('#addLibDropdownMenu').removeClass('show');
+        return false;
+    });
+
+    $(document).on('click', '.addLibListSelect', function (e) {
+        e.preventDefault();
+        var url = $(this).data('url');
+        updateLibUrl(url, false);
+        $('#addLibDropdownMenu').removeClass('show');
+        $('#addLibList .show').removeClass('show');
+
+        return false;
+    });
+
+    $(document).on('click', ' .remove-lib', function (e) {
+        e.preventDefault();
+        var url = $(this).data('url');
+        console.log(url);
+        updateLibUrl(url, true);
+        $('#addLibDropdownMenu').removeClass('show');
+        return false;
+    });
+
+    function generateActiveLibList(urls) {
+        o = urls;
+        $('#addLibDropdownMenu .remove-lib').remove();
+        _.forEach(urls, function (url) {
+
+            var name;
+            var lib = _.chain(libs).flatMapDepth().filter({ url: url }).head().value();
+            if (lib) {
+                name = lib['name'];
+            } else {
+                name = url.match(/\/([^\/]+)$/).pop();
+            }
+            $('#addLibDropdownMenu').append($(`<a class="dropdown-item remove-lib" role="presentation" data-url="${url}" href="#"><i class="fa fa-close"></i> ${name}</a>`));
+        });
+    }
+
+    function updateHistoryList() {
+        $('#historyList').html('');
+        $.getJSON(`${file_code}/history`, function (data) {
+            _.forEach(data, function (d) {
+                var id = d['id'];
+                var $item;
+                if (d['id'] == file_code) {
+                    $item = $(`<span class="dropdown-item-text text-white bg-primary" role="presentation">${d['id']}</span>`);
+
+                } else {
+                    $item = $(`<a class="dropdown-item" role="presentation" href="/${d['id']}">${d['id']}</a>`);
+                }
+
+                $('#historyList').append($item)
+            });
+        });
+
+
+
+    }
+
+    updateHistoryList();
+
+    $('#share').click(function () {
+        $('#shareLink').text(location.href);
+    });
+
+    $('#addLibDropdown > .dropdown-toggle').click(function () {
+        $('#addLibList .show').removeClass('show');
+    });
+});
