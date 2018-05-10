047a4723ed968677901ba8e3a6dd31e67e7d54b0
hosokawa.takahiro <vwxyzzy@gmail.com>
2018-05-10 22:12:44 +0900

    heroku support

--- index.js
+++ index.js
@@ -137,7 +137,8 @@ app.get('/:file_code/history', function (req, res) {
   });
 
 });
-var server = app.listen(3000, function () {
+
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
